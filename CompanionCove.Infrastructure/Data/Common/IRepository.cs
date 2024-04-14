aaf5c1943b19459d9334b43c7a2cd5e87d040108
Sprooze <akula300@abv.bg>
2024-04-14 18:23:17 +0300

    Add and SaveChanges implemented in Repository

--- CompanionCove.Infrastructure/Data/Common/IRepository.cs
+++ CompanionCove.Infrastructure/Data/Common/IRepository.cs
@@ -10,5 +10,9 @@ namespace CompanionCove.Infrastructure.Data.Common
     {
         IQueryable<T> All<T>() where T : class;
         IQueryable<T> AllReadOnly<T>() where T : class;
+
+        Task AddAsync<T>(T entity) where T : class;
+
+        Task<int> SaveChangesAsync();
     }
 }

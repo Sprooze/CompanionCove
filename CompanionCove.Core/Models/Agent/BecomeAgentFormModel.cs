5de2640918a2a3a7771611ad29a1a554b57a4860
Sprooze <akula300@abv.bg>
2024-04-14 18:40:26 +0300

    BecomeAgentModel created

--- CompanionCove.Core/Models/Agent/BecomeAgentFormModel.cs
+++ CompanionCove.Core/Models/Agent/BecomeAgentFormModel.cs
@@ -1,6 +1,14 @@
-﻿namespace CompanionCove.Core.Models.Agent
+﻿using System.ComponentModel.DataAnnotations;
+using static CompanionCove.Core.Constants.MessageConstants;
+using static CompanionCove.Infrastructure.Constants.DataConstants;
+namespace CompanionCove.Core.Models.Agent
 {
 	public class BecomeAgentFormModel
 	{
+		[Required(ErrorMessage = RequiredMessage)]
+		[StringLength(MaxAgentPhoneLength, MinimumLength = MinAgentPhoneLength,ErrorMessage = LengthMessage)]
+		[Display(Name = "Phone number")]
+		[Phone]
+		public string PhoneNumber { get; set; } = null!;
 	}
 }
